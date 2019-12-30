18f8406b23cbf615e8621714325fe2b0d48c588e
Jeroen de Bruijn <jeroen@alphega.nl>
2019-12-30 08:30:57 +0100

    Basic non persistant todolist

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-  <Todos v-bind:todos="todos" />
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos';
+  import Header from './components/layout/Header';
+  import Todos from './components/Todos';
+  import AddTodo from './components/AddTodo';
 
 export default {
   name: 'app',
   components: {
-    Todos
-
+    Header,
+    Todos,
+    AddTodo
   },
   data() {
     return {
@@ -30,6 +35,14 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    }
   }
 }
 </script>
@@ -44,5 +57,18 @@ export default {
   body {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
   }
 </style>
